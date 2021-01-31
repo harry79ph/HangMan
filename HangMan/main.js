319783c477deeb8edcb7ba936522a73355fd011c
harry79ph <73724613+harry79ph@users.noreply.github.com>
2021-01-31 23:29:50 +0000

    Update main.js
    minor changes

--- HangMan/main.js
+++ HangMan/main.js
@@ -1,90 +1,60 @@
-const body = document.querySelector('body');
-const colour = document.querySelector('#colour');
 const second = document.querySelector('.second');
 const wrong = document.querySelector('#wrong');
 const guess = document.querySelector('#guess');
 const wins = document.querySelector('#wins');
 const man = document.querySelector('.man');
-var choice;
-var word;
-var display;
-var win = 0;
-var w;
-var g;
-colour.addEventListener('click', e => {
-    if (e.target.tagName === 'IMG') {
-        switch (e.target.className) {
-            case 'grey':
-                body.setAttribute('style', 'background-color: grey;');
-                break;
-            case 'yellow':
-                body.setAttribute('style', 'background-color: yellow;');
-                break;
-            case 'red':
-                body.setAttribute('style', 'background-color: red;');
-                break;
-            case 'green':
-                body.setAttribute('style', 'background-color: green;');
-                break;
-            case 'blue':
-                body.setAttribute('style', 'background-color: blue;');
-                break;
-            case 'white':
-                body.setAttribute('style', 'background-color: white;');
-                break;
-            default:
-                break;
-        }
-    }
-});
-init();
-function init() {
-    const words = ['food', 'drink', 'milk', 'apple', 'butterfly', 'fish', 'donkey', 'green', 'yellow', 'black',
-            'computer', 'phone', 'printer', 'pencil', 'rubber', 'face', 'arm', 'hand', 'flower', 'tree'];
-    const random = Math.floor(Math.random() * 20);
-    choice = words[random];
-    word = Array.from(choice);
-    display = [];
-    w = 0;
-    g = 10;
-    for (let i = 0; i < word.length; i++) {
-        display.push('_');
-    }
-    wrong.innerHTML = `${w}`;
-    guess.innerHTML = `${g}`;
-    second.innerHTML = `${display.join(' ')}`;
-    man.innerHTML = `<img src="imgs/${w}.png" alt="man">`;
+let choice;
+let word;
+let display;
+let win = 0;
+let w;
+
+function init() { //resets all the values except win every round
+  const words = ['food', 'drink', 'milk', 'apple', 'dog', 'fish', 'donkey', 'green', 'yellow', 'computer'];
+  const random = Math.floor(Math.random() * 10); //gives a number between 0 - 9
+  choice = words[random]; //pickes a random array element which is a string
+  console.log(choice);
+  word = Array.from(choice);
+  display = []; //character array will be displayed in second
+  w = 0;
+  for (let i = 0; i < word.length; i++) {
+    display.push('_');
+  }
+  wrong.innerHTML = `${w}`;
+  guess.innerHTML = `${10-w}`;
+  second.innerHTML = `${display.join(' ')}`;
+  man.innerHTML = `<img src="imgs/${w}.png" alt="man">`;
 }
-counter++;
-window.addEventListener('keydown', e => {
-    keyMatch(e.key);
+init();
+window.addEventListener('keydown', e => { //gets the pressed key
+  let char = e.key.toLowerCase();
+  if (/^[a-z]$/.test(char)) {
+    keyMatch(char);
+  }
 });
-const keyMatch = (key) => {
-    if (choice.includes(key)) {
-        word.forEach((letter, index) => {
-            if (letter === key) {
-                display[index] = key;
-            }
-        });
-        second.innerHTML = `${display.join(' ')}`;
-        if (!display.includes('_')) {
-            win++;
-            wins.innerHTML = `${win}`;
-            alert(`Yes it's ${choice}. You won!!!`);
-            init();
-        }
-    } else {
-        w++;
-        wrong.innerHTML = `${w}`;
-        g--;
-        guess.innerHTML = `${g}`;
-        man.innerHTML = `<img src="imgs/${w}.png" alt="man">`;
-        
+const keyMatch = (key) => { //checks the pressed key
+  if (choice.includes(key)) {
+    word.forEach((letter, index) => {
+      if (letter === key) {
+        display[index] = key;
+      }
+    });
+    console.log(display);
+    second.innerHTML = `${display.join(' ')}`;
+    if (!display.includes('_')) {
+      win++;
+      wins.innerHTML = `${win}`;
+      alert(`Yes it's ${choice}. You won!!!`);
+      init();
     }
+  } else {
+    w++;
+    wrong.innerHTML = `${w}`;
+    guess.innerHTML = `${10-w}`;
+    man.innerHTML = `<img src="imgs/${w}.png" alt="man">`;
     if (w === 10) {
-        setTimeout(() => {
-            alert(`It was ${choice}. You lost!!!`);
-        }, 100);
-        init();
+      alert(`It was ${choice}. You lost!!!`);
+      init();
     }
+  }
 }
