92777d74160d0c57d7ca0d43dbd3da17556c43f5
harry79ph <73724613+harry79ph@users.noreply.github.com>
2021-02-27 20:06:58 +0000

    Update main.js

--- HangMan/main.js
+++ HangMan/main.js
@@ -3,57 +3,54 @@ const wrong = document.querySelector('#wrong');
 const guess = document.querySelector('#guess');
 const wins = document.querySelector('#wins');
 const man = document.querySelector('.man');
-let choice;
-let word;
-let display;
-let win = 0;
-let w;
+const message = document.querySelector('.message')
+let choice, word, display, w, win = 0;
 
-function init() { //resets all the values except win every round
+function init() {
   const words = ['food', 'drink', 'milk', 'apple', 'dog', 'fish', 'donkey', 'green', 'yellow', 'computer'];
-  const random = Math.floor(Math.random() * 10); //gives a number between 0 - 9
-  choice = words[random]; //pickes a random array element which is a string
-  console.log(choice);
+  const random = Math.floor(Math.random() * 10);
+  choice = words[random];
   word = Array.from(choice);
-  display = []; //character array will be displayed in second
+  display = [];
   w = 0;
-  for (let i = 0; i < word.length; i++) {
-    display.push('_');
-  }
+  display = word.map(item => '_');
   wrong.innerHTML = `${w}`;
-  guess.innerHTML = `${10-w}`;
+  guess.innerHTML = `${10 - w}`;
   second.innerHTML = `${display.join(' ')}`;
   man.innerHTML = `<img src="imgs/${w}.png" alt="man">`;
 }
 init();
-window.addEventListener('keydown', e => { //gets the pressed key
+window.addEventListener('keydown', e => {
   let char = e.key.toLowerCase();
-  if (/^[a-z]$/.test(char)) {
-    keyMatch(char);
-  }
+  if (/^[a-z]$/.test(char)) keyMatch(char);
 });
-const keyMatch = (key) => { //checks the pressed key
+const keyMatch = (key) => {
   if (choice.includes(key)) {
     word.forEach((letter, index) => {
-      if (letter === key) {
-        display[index] = key;
-      }
+      if (letter === key) display[index] = key;
     });
-    console.log(display);
     second.innerHTML = `${display.join(' ')}`;
     if (!display.includes('_')) {
       win++;
       wins.innerHTML = `${win}`;
-      alert(`Yes it's ${choice}. You won!!!`);
+      message.innerText = `Yes it's ${choice}. You won!!!`;
+      message.classList.add('active');
+      setTimeout(() => {
+        message.classList.remove('active');
+      }, 2000);
       init();
     }
   } else {
     w++;
     wrong.innerHTML = `${w}`;
-    guess.innerHTML = `${10-w}`;
+    guess.innerHTML = `${10 - w}`;
     man.innerHTML = `<img src="imgs/${w}.png" alt="man">`;
     if (w === 10) {
-      alert(`It was ${choice}. You lost!!!`);
+      message.innerText = `It was ${choice}. You lost!!!`;
+      message.classList.add('active');
+      setTimeout(() => {
+        message.classList.remove('active');
+      }, 2000);
       init();
     }
   }
